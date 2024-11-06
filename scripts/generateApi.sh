142ecb6c3e304433e4dcae7c96cacd7bc9c93e88
Salama Ashoush <salamaashoush@gmail.com>
2024-11-06 02:46:45 +0100

    add read me

--- scripts/generateApi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-export TS_POST_PROCESS_FILE="npx prettier --write"
-
-npx openapi-generator-cli generate -i https://api-dev.conix.ai/api/conix-ai/v3/api-docs -g typescript-axios -o app/api/generated --skip-validate-spec --enable-post-process-file --additional-properties="supportsES6=true,useSingleRequestParameter=true,withInterfaces=true,withNodeImports=true,withSeparateModelsAndApi=true,modelPackage=models,apiPackage=api,typescriptThreePlus=true"
