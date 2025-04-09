5d228599a96f33c201ba8bbaacb3f81b4fbbcf24
Mohammed Yaqob <mohammed.yaqob.98@gmail.com>
2025-04-09 15:38:54 +0200

    refactor: use valibot env lib instead of hard coded env util

--- app/env.ts
+++ app/env.ts
@@ -1,46 +1,29 @@
 import * as v from "valibot";
+import { createEnv } from "valibot-env";
 
-// Define server-only schema
-const serverEnvSchema = v.object({
-  NODE_ENV: v.optional(v.string(), "development"),
-  API_URL: v.string(),
-  SESSION_SECRET: v.string(),
-});
-
-// Define client schema
-const viteEnvSchema = v.object({
-  MODE: v.optional(v.string(), "development"),
-  BASE_URL: v.optional(v.string(), "/"),
-  PROD: v.optional(v.boolean(), false),
-  DEV: v.optional(v.boolean(), true),
-  SSR: v.optional(v.boolean(), false),
-});
-
-// Validate and parse environment variables
-const parsedServerEnv =
-  // eslint-disable-next-line no-restricted-properties
-  import.meta.env.SSR ? v.parse(serverEnvSchema, process.env) : {};
-const parsedClientEnv = v.parse(viteEnvSchema, import.meta.env);
-
-type ParsedServerEnv = v.InferOutput<typeof serverEnvSchema>;
-type ParsedClientEnv = v.InferOutput<typeof viteEnvSchema>;
-type ParsedEnv = ParsedServerEnv & ParsedClientEnv;
-
-// Merge parsed environments, with server env hidden from client
-export const env = new Proxy(
-  import.meta.env.SSR
-    ? { ...parsedClientEnv, ...parsedServerEnv }
-    : parsedClientEnv,
-  {
-    get(target, prop) {
-      if (prop in parsedServerEnv && typeof window !== "undefined") {
-        throw new Error(
-          `Access to server-only environment variable '${String(prop)}' from client code is not allowed.`,
-        );
-      }
-      return prop in parsedServerEnv
-        ? parsedServerEnv[prop as keyof typeof parsedServerEnv]
-        : target[prop as keyof typeof parsedClientEnv];
+export const env = createEnv({
+  publicPrefix: "VITE_",
+  schema: {
+    private: {
+      API_URL: v.string(),
+      SESSION_SECRET: v.string(),
+    },
+    shared: {
+      NODE_ENV: v.optional(
+        v.union([v.literal("development"), v.literal("production")]),
+        "development",
+      ),
+      // vite env
+      MODE: v.optional(v.string(), "development"),
+      BASE_URL: v.optional(v.string(), "/"),
+      PROD: v.optional(v.boolean(), false),
+      DEV: v.optional(v.boolean(), true),
+      SSR: v.optional(v.boolean(), false),
     },
   },
-) as ParsedEnv;
+  values: {
+    // eslint-disable-next-line no-restricted-properties
+    ...process.env,
+    ...import.meta.env,
+  },
+});
